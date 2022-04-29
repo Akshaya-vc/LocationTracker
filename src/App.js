d715f0e27e01e7e6ca382c447f8fdf77a065584d
Akshaya-vc <akshayav7c@gmail.com>
2022-04-29 21:56:27 +0530

    add: first iteration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+
 
 function App() {
+  const [info, setInfo] = useState(false);
+  const [data, setData] = useState({});
+  const handleClick = () => {
+    fetch("http://localhost:8000/testAPI")
+      .then((res) => res.text())
+      .then((res) => setData(JSON.parse(res)));
+    setInfo(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Location Tracker</h1>
+      <Button variant="contained" onClick={handleClick}>
+        Track packet
+      </Button>
+      {info ? (
+        <div>
+          <div className="item">
+            <h4> IP address:</h4>&emsp; <p> {data.ip}</p>
+          </div>
+          {/* <div className="item">
+            <h4> Route:</h4>&emsp; <p> {data.asn.route}</p>
+          </div> */}
+          <div className="item">
+            <h4> Continent:</h4>&emsp; <p> {data.continent_name}</p>
+          </div>
+          <div className="item">
+            <h4> Latitude:</h4>&emsp; <p> {data.latitude}</p>
+          </div>
+          <div className="item">
+            <h4> Longitude:</h4>&emsp; <p> {data.longitude}</p>
+          </div>
+          <div className="item">
+            <h4> Country name:</h4>&emsp; <p> {data.country_name}</p>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
